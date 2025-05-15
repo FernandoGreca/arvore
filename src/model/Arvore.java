c87a2dc6dbac13ebb5c78401ef53ae85db683cd0
FernandoGreca <fernandobgeca@gmail.com>
2025-05-15 20:17:01 -0300

    Ajuste na implementacao da classe arvore

--- src/model/Arvore.java
+++ src/model/Arvore.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Arvore {
+    private No primeiro;
+
+    public Arvore(String conteudo) {
+        this.primeiro = new No(conteudo);
+    }
+
+    public No getPrimeiro() {
+        return primeiro;
+    }
+
+    public void setPrimeiro(No primeiro) {
+        this.primeiro = primeiro;
+    }
     
 }
